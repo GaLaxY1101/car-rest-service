6e41d15d9595b19bd3a826f60c01f2fe2675a77f
Illia Korniichyk <korniykhik3@gmail.com>
2024-10-16 16:26:10 +0300

    Init commit

---
+++ car-rest-service/src/test/java/com/foxminded/korniichyk/car_rest_service/CarRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foxminded.korniichyk.car_rest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
